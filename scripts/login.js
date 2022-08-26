8defae3cef4db88ae5c9ead7da7c71635e518020
akashr1122 <akashsingh4321a@gmail.com>
2022-08-26 12:51:36 +0530

    add file

--- scripts/login.js
+++ scripts/login.js
@@ -4,14 +4,16 @@
 
 function signup(){
     window.location.href="signup1.html"
-}
- let arr = JSON.parse(localStorage.getItem("signupData"))
- console.log(arr)
+} function login(){
+   
+let present = 0
 
- function login(){
+let arr = JSON.parse(localStorage.getItem("signupData"))
+         console.log(arr)
+      
     let email1 = document.getElementById("email").value
     let password1 = document.getElementById("password").value
-          let present = 0
+         
     arr.forEach(({email,password})=>{
      
      if(email1 == email && password1 == password){
@@ -20,10 +22,11 @@ function signup(){
      }
 })
    if(present==1){
-        window.location.href="index.html"
+       
          let obj = {}
          obj.img ="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQqOA_oBdBM8ofNuI3Wrdz8IEnIJP4j6kAQoA&usqp=CAU"
          localStorage.setItem("objimg",JSON.stringify(obj))
+         window.location.href="index.html"
    }else{
     document.getElementById("invalid").style.display="block"
    }
